f86265c3b7cde2847fd0cb1848074445f50806db
Stevdza-San <stefan.jovanavich@gmail.com>
2023-07-17 08:54:25 +0200

    Java updated to 17.

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -1,6 +1,6 @@
 object ProjectConfig {
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val minSdk = 24
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val extensionVersion = "1.4.0"
 }
